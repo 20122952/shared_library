d34d04a7937a7f51dca260da4fc6f13a943570d2
zhefeng.ning <zhefeng.ning@shopee.com>
2022-07-07 18:27:35 +0800

    edit method

---
+++ src/com/ning/TestDetail.java
@@ -0,0 +1,7 @@
+package com.ning;
+
+public class TestDetail {
+    public String addTestDetails(String text){
+        return "asd123" + text + "=asdads";
+    }
+}
--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,12 @@
 #!/usr/bin/env groovy
+import com.ning.TestDetail
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  // test = env.TEST
+  test = new TestDetail().addTestDetails(${name})
+  println(test)
 }
+
+
 
