9587a5da3568bb8aa97b483d6a59c4edc6055cfa
zhefeng.ning <zhefeng.ning@shopee.com>
2022-07-07 18:51:29 +0800

    edit method

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
-import com.ning.TestDetail
+// import com.ning.TestDetail
 
 def exe(){
     stage('Demo') {
         testname = "${env.TEST}"
-        test = new TestDetail().addTestDetails(testname)
+        // test = new TestDetail().addTestDetails(testname)
         println(test)
     }
 }
