ed9d6d43b413cded13b2324ac2c7667409207087
zhefeng.ning <zhefeng.ning@shopee.com>
2022-07-07 18:37:51 +0800

    edit method

--- src/com/ning/TestDetail1.groovy
+++ src/com/ning/TestDetail1.groovy
@@ -1,6 +1,6 @@
 package com.ning
 
-class TestDetail {
+class TestDetail implements Serializable {
     public String addTestDetails(String text){
         return "asd123" + text + "=asdads";
     }
